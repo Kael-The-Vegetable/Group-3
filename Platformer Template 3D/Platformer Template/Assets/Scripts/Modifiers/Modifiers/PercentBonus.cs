90d740c28319fe7d7071d8dd69645e01930c6e0f
JDoddsNAIT <144161210+JDoddsNAIT@users.noreply.github.com>
2024-12-20 16:44:24 -0700

    some modifiers can now be modified

--- Platformer Template 3D/Platformer Template/Assets/Scripts/Modifiers/Modifiers/PercentBonus.cs
+++ Platformer Template 3D/Platformer Template/Assets/Scripts/Modifiers/Modifiers/PercentBonus.cs
@@ -9,19 +9,18 @@ public class PercentBonus : IModifier<float>, IModifier<int>
     int IModifier<int>.Order => 0;
 
     [field: SerializeField, Tooltip("The percentage of the base value that is added to it.")]
-    
-    public float Percentage { get; set; }
+    public Moddable<float> Percentage { get; set; }
 
-    public PercentBonus(float percentage) => Percentage = percentage;
+    public PercentBonus(float percentage) => Percentage.BaseValue = percentage;
 
     float IModifier<float>.ApplyModifier(float baseValue, float modValue)
     {
-        return modValue + Percentage / 100f * baseValue;
+        return modValue + Percentage.ModifiedValue / 100f * baseValue;
     }
 
     int IModifier<int>.ApplyModifier(int baseValue, int modValue)
     {
-        return (int)(modValue + Percentage / 100f * baseValue);
+        return (int)(modValue + Percentage.ModifiedValue / 100f * baseValue);
     }
 
     public static explicit operator PercentBonus(float baseValue) => new(baseValue);
